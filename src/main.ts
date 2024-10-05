b75f5b694593b096d57fc2184ba2001225083c6c
corvuscorae <corvuscorae@gmail.com>
2024-10-04 22:37:26 -0700

    step 1: a button you can click

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const buttonText = "🐜";
+const button = document.createElement("button");
+button.innerHTML = buttonText;
+app.append(button);
