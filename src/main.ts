59731a7a2b0a7c30f8fb57af3b01f614a7df51af
corvuscorae <corvuscorae@gmail.com>
2024-10-04 23:02:09 -0700

    step 2: clicking increases counter

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,12 @@ const buttonText = "🐜";
 const button = document.createElement("button");
 button.innerHTML = buttonText;
 app.append(button);
+
+let counter: number = 0;
+const countDisplay = document.createElement("div");
+countDisplay.innerHTML = `ants: ${counter}`;
+button.addEventListener("click", () => {
+    counter++;
+    countDisplay.innerHTML = `ants: ${counter}`;
+});
+app.append(countDisplay);
